86ca89bdc72084be2582f2a8dac98f69e158753b
Michael Maurice <michaelmaurice92@gmail.com>
2019-05-06 02:26:31 +0000

    Made some minor changes

--- main.c
+++ main.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
-int LrgValue(int *x);
+
 
 int main(){
-    int arrayData[3][3];
-    int row, col;
+    int arrX = 3, arrY = 3; //arrayData row and col size 
+    int arrayData[arrX][arrY]; //array used for data storage
+    int row, col; 
     int max = 0;
-    FILE *input;
-    input = fopen("input.txt", "r");
+    FILE *input; //File input pointer
+    input = fopen("input.txt", "r"); //input is read from file
     
     //scans input file for integers and stores then in a 3x3 array
-    for(row = 0; row < 3; row++){
-        for(col = 0; col < 3; col++){
+    for(row = 0; row < arrX; row++){
+        for(col = 0; col < arrY; col++){
             
         fscanf(input,"%d", &arrayData[row][col]);
         
@@ -18,8 +19,8 @@ int main(){
     }
     
     //prints array
-    for(row = 0; row < 3; row++){
-        for(col = 0; col < 3; col++){
+    for(row = 0; row < arrX; row++){
+        for(col = 0; col < arrY; col++){
             
         printf("%d ", arrayData[row][col]);
         }
@@ -27,8 +28,8 @@ int main(){
     }
     
     //finds and records max value in array
-    for(row = 0; row < 3; row++){
-        for(col = 0; col < 3; col++){
+    for(row = 0; row < arrX; row++){
+        for(col = 0; col < arrY; col++){
           
             if(arrayData[row][col] > max){
                 max = arrayData[row][col];
@@ -36,6 +37,8 @@ int main(){
         } 
     }
     printf("\nmax value in 3x3 array is %d\n", max);
+    
+    fclose(input);
     
     return 0;
 }
